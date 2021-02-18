aef2b5738a9459cd3de95f01918af3b24698c2ec
blackjac7 <febriansyah705@gmail.com>
2021-02-18 18:32:34 +0700

    add generateToken-helpers & migration and asociation Tables & User login/register API

---
+++ models/room.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Room extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Room.belongsToMany(models.User, {
+        through: models.UserRoom
+      })
+    }
+  };
+  Room.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: "Name is required"
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Room',
+  });
+  return Room;
+};
